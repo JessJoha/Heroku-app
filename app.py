37467d6c8ac9c046cf2e63446f264c4c298d8f5f
JessJoha <jessicajcenteno@gmail.com>
2025-05-14 17:59:15 -0500

    🐛 🐛  update files

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Hola desde el puerto 3003!'
+    return '¡Hola desde Heroku en Docker!'
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 3003))  # Heroku pondrá el puerto en PORT
+    port = int(os.environ.get("PORT", 3003))
     app.run(host='0.0.0.0', port=port)
