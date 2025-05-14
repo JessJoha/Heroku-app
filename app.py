29663ef37553d4f772bdd62c0bf1af92788c7cac
JessJoha <jessicajcenteno@gmail.com>
2025-05-14 18:37:29 -0500

    coments

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '¡Hola desde Heroku en Docker!'
+    return '¡Hello word !'
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3003))
